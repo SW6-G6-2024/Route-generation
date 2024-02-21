cf7fa3245f766024fab76d33d98c666b4ce8194c
MortenMunk <mortenmunk97@gmail.com>
2024-02-21 09:51:43 +0100

    hotfix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
+from data_formatter import data_formatter
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -6,22 +7,8 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/svm', methods=['GET','POST'])
+@app.route('/test', methods=['GET','POST'])
 def hello_world_post():
     request_data = request.get_json().get('data', "No data found")
     
-    return jsonify({'data': request_data})
-
-
-@app.route('/bert', methods=['GET','POST'])
-def hello_world_post():
-    request_data = request.get_json().get('data', "No data found")
-    
-    return jsonify({'data': request_data})
-
-
-@app.route('/lr', methods=['GET','POST'])
-def hello_world_post():
-    request_data = request.get_json().get('data', "No data found")
-    
-    return jsonify({'data': request_data})
+    return data_formatter(request_data)
