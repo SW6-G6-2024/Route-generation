504bab57d21042759632b1a1f713c78f3f65536e
Morten Munk <92527083+MortenMunk@users.noreply.github.com>
2024-03-14 10:19:34 +0100

    [MERGE] Dev to test (#14)
    * Djikstras algo implemented (#4)
    
    * Create route_creator.py
    
    * Potential nice graph created
    
    * Finds nearest nodes within 50 m or nearest node
    
    Finds several nodes for each piste or lift if they are both inside the 50 m threshold.
    
    * Creates a nice graph
    
    * Djikstra's algorithm implemented
    
    Somestimes it doesn't find a route...
    
    * Finds all nodes within 50 m
    
    * Turn the calulated route into geoJSON
    
    * Update route_creator.py
    
    * Longer route
    
    * feat: Added mock route for generating route
    
    Changes made:
    - Added mock route for generating route
    - Updated Dockerfile to run on port 3500
    
    * Refactoring of code
    
    ---------
    
    Co-authored-by: Jacob Terpe Woods <jacobterpewoods@gmail.com>
    
    * Returns shortest route on route /generate-route (#5)
    
    * Returns shortest route on route /generate-route
    
    * Gets pistedata from BE
    
    * Descriptions for all functions and indent fix
    
    * Restructuring and tests
    
    * refactor: route_creator for better code health
    
    * fix: GitHub actions
    
    * Update pylint.yml
    
    * fix: removed pylint
    
    * Update requirements.txt
    
    ---------
    
    Co-authored-by: Jacob Terpe Woods <jacobterpewoods@gmail.com>
    
    * fix: Refactoring + tests fixed (#11)
    
    * refactor: split functions into several files + added comments
    
    * Tests passes and runnable
    
    * docs: Added documentation to functions + moved classes
    
    * fix: removed relative import from main.py
    
    ---------
    
    Co-authored-by: Mafusn ❤️ <Magnus.peetz.holt@gmail.com>
    
    * removed pylint
    
    * removed hello world tests
    
    ---------
    
    Co-authored-by: Mafusn <92912046+Mafusn@users.noreply.github.com>
    Co-authored-by: Jacob Terpe Woods <jacobterpewoods@gmail.com>
    Co-authored-by: Mafusn ❤️ <Magnus.peetz.holt@gmail.com>

---
+++ src/route_creation/dijkstra.py
@@ -0,0 +1,54 @@
+import heapq
+from .classes import DijkstraData
+
+def dijkstra(graph:dict, start:int, end:int):
+	"""
+ Find the shortest path between two nodes in a graph using Dijkstra's algorithm based on some weight.
+
+	Args:
+		graph (dict): The graph to search for the shortest path
+		start (int): The id of the start node
+		end (int): The id of the end node
+
+	Returns:
+		dict, float: A list of node ids representing the shortest path, and the distance of the shortest path in kilometers
+	"""
+	dijkstra_data = DijkstraData(start, graph)
+	
+	while dijkstra_data.priority_queue:
+		# Get the node with the smallest distance
+		current_distance, current_node = heapq.heappop(dijkstra_data.priority_queue)
+		
+		# If we've reached the end node, we can stop
+		if current_node == end:
+			break
+		
+		explore_neighbors(graph, current_node, current_distance, dijkstra_data)
+
+	# Reconstruct the shortest path
+	path = []
+	current = end
+	while current is not None:
+		path.append(current)
+		current = dijkstra_data.previous_nodes[current]
+	path.reverse()
+	
+	return path, dijkstra_data.distances[end]
+	
+def explore_neighbors(graph:dict, current_node: int, current_distance: float, dijkstra_data: DijkstraData):
+	"""Searches for the shortest path to the neighbors of the current node.
+
+	Args:
+			graph (dict): The graph to search for the shortest path
+			current_node (int): The id of the current node
+			current_distance (float): The distance to the current node
+			dijkstra_data (DijkstraData): The data structures used in Dijkstra's algorithm. Contains the distances, previous nodes, and priority queue
+	"""
+	for neighbor, weight in graph[current_node]:
+		distance = current_distance + weight
+			
+		# If the distance to the neighbor is shorter by taking this path
+		if distance < dijkstra_data.distances[neighbor]:
+			dijkstra_data.distances[neighbor] = distance
+			dijkstra_data.previous_nodes[neighbor] = current_node
+			heapq.heappush(dijkstra_data.priority_queue, (distance, neighbor))
---
+++ src/route_creation/route_creator.py
@@ -0,0 +1,106 @@
+from .dijkstra import dijkstra
+from .graph import create_graph, find_connections_for_stranded_nodes, find_stranded_node_coordinates
+from .haversine import haversine
+
+
+def get_shortest_path_geojson(filtered_data:dict, shortest_path:list, shortest_distance:float):
+    """Converts the shortest path to a GeoJSON FeatureCollection.
+
+    Args:
+        filtered_data (dict): The filtered GeoJSON data from the Overpass API
+        shortest_path (list): The list of node IDs in the shortest path
+        shortest_distance (float): The distance of the shortest path
+
+    Returns:
+        dict: A GeoJSON FeatureCollection representing the shortest path
+    """
+    
+    # Create a lookup table for node IDs to their coordinates
+    node_id_to_coords = {}
+    for element in filtered_data['elements']:
+        for i, node_id in enumerate(element['nodes']):
+            lat, lon = element['geometry'][i]['lat'], element['geometry'][i]['lon']
+            node_id_to_coords[node_id] = (lat, lon)
+
+    # Initialize an empty GeoJSON FeatureCollection
+    geojson_data = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    # Check if there is a shortest path to convert
+    if shortest_path:
+        # Extract coordinates from the node IDs in the shortest path
+        path_coordinates = [node_id_to_coords.get(node_id, ("Unknown", "Unknown")) for node_id in shortest_path]
+
+        # Ensure coordinates are not 'Unknown' before attempting to switch to avoid errors
+        path_coordinates = [(lon, lat) for lat, lon in path_coordinates if (lat, lon) != ("Unknown", "Unknown")]
+
+        # Create a GeoJSON Feature for the LineString representing the shortest path
+        path_feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": path_coordinates
+            },
+            "properties": {
+                "description": "Shortest Path",
+                "distance_km": shortest_distance,
+                "piste:type": "downhill"
+            }
+        }
+
+        # Add the path Feature to the FeatureCollection
+        geojson_data["features"].append(path_feature)
+
+    return geojson_data
+    
+
+def generate_shortest_route(start: dict[float,float], end: dict[float,float], overpassData: dict):
+  """Generates the shortest route between two points using the Dijkstra algorithm.
+
+  Args:
+      start (dict[float,float]): The coordinates of the start point
+      end (dict[float,float]): The coordinates of the end point
+      overpassData (dict): The GeoJSON data from the Overpass API
+
+  Returns:
+      dict: A GeoJSON FeatureCollection representing the shortest path
+  """
+  
+  
+  filtered_data = overpassData
+
+  if 'elements' in filtered_data and len(filtered_data['elements']) <= 0:
+    print("No elements found in the filtered_data")
+
+  graph = create_graph(filtered_data)
+  graph = find_connections_for_stranded_nodes(graph, filtered_data)
+  start_node = find_nearest_node(start, graph, filtered_data)
+  end_node = find_nearest_node(end, graph, filtered_data)
+
+  shortest_path, shortest_distance = dijkstra(graph, start_node, end_node)
+
+  # Use the function and print the GeoJSON data
+  geojson_data = get_shortest_path_geojson(filtered_data, shortest_path, shortest_distance)
+
+  return geojson_data
+
+def find_nearest_node(coords, graph, elements):
+    """Finds the id of the nearest node in a graph to the given coordinates.
+
+    Args:
+        coords (dict[float,float]): The coordinates to find the nearest node to
+
+    Returns:
+        int: The id of the nearest node
+    """
+    nearest_node = None
+    min_distance = float('inf')
+    for node_id, node_coords in graph.items():
+        lat, lon = find_stranded_node_coordinates(node_id, elements)
+        distance = haversine(coords.get('lat'), coords.get('lon'), lat, lon)
+        if distance < min_distance:
+            min_distance = distance
+            nearest_node = node_id
+    return nearest_node
